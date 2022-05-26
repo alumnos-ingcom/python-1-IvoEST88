391c99773e6efd1d827ded1ccbcca39d350615c1
Ivo <ivoexesosa88@gmail.com>
2022-05-26 18:34:22 -0300

    Arreglos: Resto siempre era positivo y si el dividendo era menor que el divisor se hacia un bucle infinito.

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -17,20 +17,23 @@ def division_lenta(dividendo, divisor):
     if dividendo <0:
         dividendo = abs(dividendo)
         signo = -1
+    elif dividendo < divisor:
+        return print("El dividendo no puede ser menor al divisor")
     elif dividendo >0 and divisor >0:
         signo = 1
     elif divisor <0:
         return print("No se puede utilizar un divisor igual o menor a 0")
+
     else:
         return print("No se puede utilizar un dividendo igual o menor a 0")
-    while dividendo >=divisor:
+    while dividendo > divisor:
         cociente += 1
         print(f"{dividendo} - {divisor}")
         dividendo -= divisor
+        dividendo *= signo
         print(f"Resto : {dividendo}")
         print("--------------")
-    dividendo *= signo
-    resto = dividendo
+    resto = dividendo 
     return cociente, resto
 def principal():
     """
