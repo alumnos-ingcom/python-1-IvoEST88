58b03734066ae4a74b66cf0f4292c61202b59a18
Ivo <ivoexesosa88@gmail.com>
2022-05-26 22:58:40 -0300

    Todos los casos en dividendo y divisor (positivo-negativo)

--- src/ejercicio5.py
+++ src/ejercicio5.py
@@ -13,27 +13,37 @@ def division_lenta(dividendo, divisor):
     Division mediante una resta sucesiva
     """
     signo=1
+    signo2=1
     cociente = 0
-    if dividendo <0:
-        dividendo = abs(dividendo)
+    if dividendo <0 and divisor >0:
+        dividendo *= -1
         signo = -1
-    elif dividendo < divisor:
-        return print("El dividendo no puede ser menor al divisor")
     elif dividendo >0 and divisor >0:
         signo = 1
-    elif divisor <0:
-        return print("No se puede utilizar un divisor igual o menor a 0")
+    elif divisor <0 and dividendo >0:
+        divisor *= -1
+        signo2 = -1
+    elif divisor <0 and dividendo <0:
+        signo = -1
+        signo2 = -1
+        dividendo *= -1
+        divisor *= -1
+    elif dividendo < divisor:
+        return print("El dividendo no puede ser menor al divisor")
     else:
-        return print("No se puede utilizar un dividendo igual o menor a 0")
-    while dividendo > divisor:
+        return print("El divisor no puede ser 0!")
+    while dividendo >= divisor:
         cociente += 1
         print(f"{dividendo} - {divisor}")
         dividendo -= divisor
-        dividendo *= signo
-        print(f"Resto : {dividendo}")
+        print(f"Resto : {signo}{dividendo}")
         print("--------------")
+    dividendo *= signo
+    divisor *= signo2
+    cociente *= signo
+    cociente *= signo2
     resto = dividendo
-    return cociente, resto
+    return print(f"Cociente: {cociente}\nResto: {resto}")
 def principal():
     """
     Programa
